edd0ae90f51dbf034cd06a6bf0c75facaae39879
LieutenantPorky <jacopo@siniscalco.eu>
2018-12-08 13:36:08 +0000

    Expanded pointMass

--- pointMass.py
+++ pointMass.py
@@ -7,16 +7,21 @@ Created on Sat Dec  8 13:03:04 2018
 """
 
 import math
-import numpy
 
 class pointMass:
     '''Class for point masses '''
     position = []
     velocity = []
     mass = 1
-    
+
     def __init__(self, position, velocity):
         self.position = position
         self.velocity = velocity
-        
-        
+        print("Initiated a point mass at ",position," with velocity ", velocity)
+
+    def broadcast(self):
+        print("point mass at", self.position," with velocity",self.velocity, "and mass ", self.mass)
+
+    def update(self, timeStep):
+        ''' Update the particle's data after time timeStep '''
+        position = [self.position[i] + self.velocity[i] * timeStep for i in [0,1]]
