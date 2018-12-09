b6db8b9d4baf379da3cc54a72c79e2fe1ca98c63
Favour <itsfavs@gmail.com>
2018-12-09 03:58:00 +0000

    Merge thing

--- cgi-bin/Main.py
+++ cgi-bin/Main.py
@@ -1,11 +1,39 @@
+#!C:\Program Files (x86)\Python37-32\python.exe -u
 from pointMass import pointMass
 import matplotlib.pyplot as plt
 import mpld3
 import Forces
 from Forces import linearForce
+from Forces import invSquareForce
 from VectorMath import vector
 
+import cgi
+import cgitb
+cgitb.enable()
+print ("Content-type:text/html\r\n\r\n")
+'''Do form analysis here'''
+
+form = cgi.FieldStorage()
+value = form.getlist("Pos1")
+pos1 = [float(i) for i in value]
+print("<p>Position:", pos1)
+
+value = form.getlist("Vel1")
+vel1 = [float(i) for i in value]
+print("<p>Velocity:", vel1)
+
+value = form.getlist("Mass1")
+mass1 = [float(i) for i in value][0]
+print("<p>Mass 1:", mass1)
+
+
+''' Run the simulation '''
+
+
+
 plt.figure(figsize = (8,6))
+fig, ax = plt.subplots()
+
 
 '''
 Test what is currently going on
@@ -15,8 +43,8 @@ Global Variables
 timestep = 0.005
 endTime = 1.5
 
-masses = [pointMass('mass 1',[0,0],[2,0.0]), pointMass('mass 2',[0,0],[1,0.0])]
-forces = [linearForce([4.0,4.0], -2)]
+masses = [pointMass('mass 1',pos1,vel1, mass1)]
+forces = [invSquareForce([4.0,4.0,1.0], -2)]
 data = []
 
 '''
@@ -24,7 +52,7 @@ Run a loop to update parameters
 '''
 
 def runSim():
-    print("Beginning sim")
+    #print("Beginning sim")
     time = 0
     while (time <= endTime):
         currentData = []
@@ -40,10 +68,13 @@ runSim()
 
 def plotPositions():
     ''' Plot each mass separately '''
-    for i in range(0, len(masses)):
-        plt.plot([dataPoint[i][1][0] for dataPoint in data], [dataPoint[i][1][1] for dataPoint in data], label = data[0][i][0] , color = "C" + str(i))
 
-    plt.plot([force.origin.toArray()[0] for force in forces if force.type == 'inverseSquare'], [force.origin.toArray()[1] for force in forces if force.type == 'inverseSquare'], "ko")
-    plt.legend()
+    for i in range(0, len(masses)):
+        ax.plot([dataPoint[i][1][0] for dataPoint in data], [dataPoint[i][1][1] for dataPoint in data], [dataPoint[i][1][2] for dataPoint in data], label = data[0][i][0] , color = "C" + str(i))
+
+    ax.plot([force.origin.toArray()[0] for force in forces if force.type == 'inverseSquare'], [force.origin.toArray()[1] for force in forces if force.type == 'inverseSquare'],[force.origin.toArray()[2] for force in forces if force.type == 'inverseSquare'], "ko")
+    ax.legend()
+
 plotPositions()
-mpld3.show()
+print(mpld3.fig_to_html(fig))
+print("<a href = ..\\> Go Back </a>")
